50936b37cb2ca0837a0afa735d42047e11e46c4a
Jack Gilmore <gilmorejackc@gmail.com>
2022-02-19 15:31:03 +0000

    Truncate dataset list item description and list dataset types

--- scripts/src/templates/dataset-item.js
+++ scripts/src/templates/dataset-item.js
@@ -1,7 +1,13 @@
-export default (data) => (
-`<dataset>
+import clip from "text-clipper";
+
+export default (data) => {
+  const resources = [... new Set(data.resources.map(function (res) { return `<span class="badge filetype-list-item">${res.format.toUpperCase()}</span>` }))].join('');
+  const clippedHtml = clip(data.notes, 450, { html: true, maxLines: 1 });
+
+  return `<dataset>
   <h3><a href="${data.url}">${data.title}</a></h3>
   <h4>${data.organization || ''}</h4>
-  <div>${data.notes || ''}</div>
+  <div class="dataset-item-description">${clippedHtml || ''}</div>
+  <div>${resources}</div>
 </dataset>`
-)
+}
