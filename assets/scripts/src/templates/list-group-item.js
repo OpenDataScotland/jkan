7fd4d9fc93f11aa57c6983c376b1910a6a8a0c63
Jack Gilmore <gilmorejackc@gmail.com>
2026-04-18 22:34:43 +0100

    refactor: Moving custom style to Bootstrap utils

--- assets/scripts/src/templates/list-group-item.js
+++ assets/scripts/src/templates/list-group-item.js
@@ -1,7 +1,7 @@
 export default (data) => (
-`<a href="${data.url}" class="list-group-item${data.selected ? ' active ' : ''}">
-  <span class="list-group-item-truncate">${data.title}</span>
-  <span class="badge">${data.count}</span>
-  ${data.selected ? '<span class="badge"><i class="fa-solid fa-times"></i></span>' : ''}
+`<a href="${data.url}" class="list-group-item${data.selected ? ' active ' : ''} list-group-item-action justify-content-between align-items-center" style="display: flex;">
+  <span class="d-inline-block text-truncate">${data.title}</span>
+  <span class="badge text-bg-secondary">${data.count}</span>
+  ${data.selected ? '<span class="badge text-bg-secondary"><i class="fa-solid fa-times"></i></span>' : ''}
 </a>`
 )
